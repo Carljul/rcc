d6a4361d5cb4d3dd9972bfd5bce710bfee6fdfc0
harvey <a-harvey@chronostep.com>
2022-09-07 21:19:13 +0800

    Add field validation

--- public/js/common.js
+++ public/js/common.js
@@ -1,3 +1,40 @@
-$(document).ready(function () {    
-    
+$(document).ready(function () {
+    // field validators
+    function onlyNumbers(value)
+    {
+        return value.replaceAll(/[^0-9]+/g, "")
+    }
+
+    function maxLength(value, max)
+    {
+        return value.substr(0,max)
+    }
+
+    // validation implementations
+    // deceased form
+    $('#deceasedForm #amount').on('input', (e) => {
+        let val = $(e.target).val()
+        $(e.target).val(onlyNumbers(val))
+    })
+
+    // lighting form
+    $('#lightingForm #amount').on('input', (e) => {
+        let val = $(e.target).val()
+        $(e.target).val(onlyNumbers(val))
+    })
+
+    // register form
+    $('#registerForm #password').on('input', (e) => {
+        let val = $(e.target).val()
+        val = onlyNumbers(val)
+        val = maxLength(val, 6)
+        $(e.target).val(val)
+    })
+
+    $('registerForm #password-confirm').on('input', (e) => {
+        let val = $(e.target).val()
+        val = onlyNumbers(val)
+        val = maxLength(val, 6)
+        $(e.target).val(val)
+    })
 });
