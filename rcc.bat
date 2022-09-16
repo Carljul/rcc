0d8f1ea187a2d8ccd5ad8b62f8c0cbd66932324a
JCs3lma <s-julcarl@chronostep.com>
2022-09-16 10:29:20 +0800

    env

--- rcc.bat
+++ rcc.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 ECHO STARTING RCC APPLICATION. . .
 START C:\xampp\xampp-control.exe
-cd "E:\secrets\rcc"
+cd "C:\xampp\htdocs\rcc"
 START chrome --new-window "http://127.0.0.1:8000"
 php artisan serve
